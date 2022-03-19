924deccb024c714adaf5ba4ef655cf8cdcaec9dd
CampbellRuthven <campbell.ruthven@hotmail.com>
2022-03-19 13:14:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
                 </div>
                 <div class="row">
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/ wp-content/themes/starter_for_josh_site/images/image1.jpg" alt="woman">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/wp-content/themes/starter_for_josh_site/images/image1.jpg" alt="woman">
                     <h4 class="article-title">A PORTRAIT</h4>
                     <p class="date">12 December 2020</p>
                     <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
                     <a class="readmore" href="#"> CONTINUE READING</a>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/ wp-content/themes/starter_for_josh_site/images/image2.jpg" alt="space suit">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/wp-content/themes/starter_for_josh_site/images/image2.jpg" alt="space suit">
                     <h4 class="article-title">A MODEL</h4>
                     <p class="date">12 December 2020</p>
                     <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
                     <a class="readmore" href="#"> CONTINUE READING</a>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/ wp-content/themes/starter_for_josh_site/images/image3.jpg" alt="map thing">
+                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/wp-content/themes/starter_for_josh_site/images/image3.jpg" alt="map thing">
                     <h4 class="article-title">A GAMES DESIGN</h4>
                     <p class="date">12 December 2020</p>
                     <p class="article-p" >STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
