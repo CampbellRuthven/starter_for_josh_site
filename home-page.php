4c75044a2e22b2097a3a4a3790146ae0c35201a1
CampbellRuthven <campbell.ruthven@hotmail.com>
2022-04-10 17:11:48 +0800

    Changed home page articles to link with WP

--- home-page.php
+++ home-page.php
@@ -17,29 +17,29 @@
         <section class="container-fluid articlesbg">
             <div class="container">
                 <div>
-                    <h3 class="text-center">LATEST ARTICLES</h3>
+                    <h3 class="text-center"><?php the_field('titleforarticles'); ?></h3>
                 </div>
                 <div class="row">
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/wp-content/themes/starter_for_josh_site/images/image1.jpg" alt="woman">
-                    <h4 class="article-title">A PORTRAIT</h4>
-                    <p class="date">12 December 2020</p>
-                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
-                    <a class="readmore" href="#"> CONTINUE READING</a>
+                <img class="img-fluid" alt=“portrait” src="<?php the_field('articleimage');?>" alt="portrait">
+                <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+                <p class="date" ><?php the_field('articledate'); ?></p>
+                <p class="article-p"><?php the_field('articleexcerpt'); ?> </p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/wp-content/themes/starter_for_josh_site/images/image2.jpg" alt="space suit">
-                    <h4 class="article-title">A MODEL</h4>
-                    <p class="date">12 December 2020</p>
-                    <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
-                    <a class="readmore" href="#"> CONTINUE READING</a>
+                <img class="img-fluid" alt=“spacesuit” src="<?php the_field('articleimagemiddle');?>" alt="space suit">
+                <h4 class="article-title"><?php the_field('articletitlemiddle'); ?></h4>
+                <p class="date" ><?php the_field('articledatemiddle'); ?></p>
+                <p class="article-p"><?php the_field('articleexcerptmiddle'); ?> </p>
+                <a href="<?php the_field('readmoremiddle'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub2/wp-content/themes/starter_for_josh_site/images/image3.jpg" alt="map thing">
-                    <h4 class="article-title">A GAMES DESIGN</h4>
-                    <p class="date">12 December 2020</p>
-                    <p class="article-p" >STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
-                    <a class="readmore" href="#"> CONTINUE READING</a>
+                <img class="img-fluid" alt=“map” src="<?php the_field('articleimageright');?>" alt="map thing">
+                <h4 class="article-title"><?php the_field('articletitleright'); ?></h4>
+                <p class="date" ><?php the_field('articledateright'); ?></p>
+                <p class="article-p"><?php the_field('articleexcerptright'); ?> </p>
+                <a href="<?php the_field('readmoreright'); ?>" class="readmore">CONTINUE READING</a>
                 </div>
             </div> <!-- row --> 
             </div> <!-- container -->       
